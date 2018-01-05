72466cd82c81edc003d70e6cbdfd5313a0218bb2
Thomas de Wulf <thomas.de.wulf@telenet.be>
2018-01-05 11:56:53 +0100

    UI

--- kangoeroes.leidingBeheer/frontend/app/tak/tak-leiding-add/tak-leiding-add.component.ts
+++ kangoeroes.leidingBeheer/frontend/app/tak/tak-leiding-add/tak-leiding-add.component.ts
@@ -3,6 +3,7 @@ import { FormGroup, FormBuilder, Validators, ValidatorFn, AbstractControl, Valid
 import { BsModalRef } from 'ngx-bootstrap/modal';
 import { DataService } from '../../data.service';
 
+// Custom validator om optionele velden te valideren
 function emailOrEmpty(validators?: (ValidatorFn | null | undefined)[] ): ValidatorFn {
   return (control: AbstractControl): { [key: string]: any } => {
     return control.value  ? Validators.compose(validators)(control) : null;
