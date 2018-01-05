cb8eb5f1aa7aa2cd9fdf999915b73e06744619de
Thomas de Wulf <thomas.de.wulf@telenet.be>
2018-01-05 11:52:02 +0100

    Form toevoegen

--- kangoeroes.leidingBeheer/frontend/app/tak/tak-leiding-add/tak-leiding-add.component.ts
+++ kangoeroes.leidingBeheer/frontend/app/tak/tak-leiding-add/tak-leiding-add.component.ts
@@ -1,7 +1,13 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators, ValidatorFn, AbstractControl, ValidationErrors } from '@angular/forms';
 import { BsModalRef } from 'ngx-bootstrap/modal';
 import { DataService } from '../../data.service';
+
+function emailOrEmpty(validators?: (ValidatorFn | null | undefined)[] ): ValidatorFn {
+  return (control: AbstractControl): { [key: string]: any } => {
+    return control.value  ? Validators.compose(validators)(control) : null;
+  };
+}
 
 @Component({
   // tslint:disable-next-line:component-selector
@@ -19,6 +25,13 @@ export class TakLeidingAddComponent implements OnInit {
   constructor(public addLeidingModalRef: BsModalRef, private fb: FormBuilder, private dataService: DataService) { }
 
   ngOnInit() {
+    this.addLeidingFormGroup = this.fb.group({
+      naam: ['', [Validators.required, Validators.minLength(2)]],
+      voornaam: ['', [Validators.required, Validators.minLength(2)]],
+      email: ['', [emailOrEmpty([Validators.email])]],
+      datumGestopt: [''],
+      leidingSinds: ['']
+    });
   }
 
   onSubmit() {
