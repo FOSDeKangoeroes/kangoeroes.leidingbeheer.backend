2821e0c64b5c6d1de6d07fa356a966be91ffabe2
Thomas de Wulf <thomas.de.wulf@telenet.be>
2018-01-05 08:40:14 +0100

    Takken sorteren op hun gegeven volgorde

--- kangoeroes.core/Data/Repositories/TakRepository.cs
+++ kangoeroes.core/Data/Repositories/TakRepository.cs
@@ -22,7 +22,7 @@ namespace kangoeroes.core.Data.Repositories
         }
         public IEnumerable<Tak> GetAll()
         {
-            return _takken.Include(x => x.Leiding).ToList();
+            return _takken.Include(x => x.Leiding).OrderBy(x => x.Volgorde).ToList();
         }
 
         public Tak FindById(int id)
