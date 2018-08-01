17e35021b5f6340bc1a0977f0cd5b9ced5c3fd45
Thomas de Wulf <thomas.de.wulf@telenet.be>
2018-08-01 10:37:04 +0200

    Make it possible to change the parent of an entry

--- kangoeroes.leidingBeheer/Models/ViewModels/TotemEntry/UpdateTotemEntryViewModel.cs
+++ kangoeroes.leidingBeheer/Models/ViewModels/TotemEntry/UpdateTotemEntryViewModel.cs
@@ -7,5 +7,6 @@ namespace kangoeroes.leidingBeheer.Models.ViewModels.TotemEntry
     public int AdjectiefId { get; set; }
     public int TotemId { get; set; }
     public DateTime DatumGegeven { get; set; }
+    public int VoorouderId { get; set; }
   }
 }
