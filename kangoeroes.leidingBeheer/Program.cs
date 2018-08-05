0882735be9ab2c11adc516af044841fe82594840
Thomas de Wulf <thomas.de.wulf@telenet.be>
2018-08-05 12:25:45 +0200

    Auto migrate on startup

--- kangoeroes.leidingBeheer/Program.cs
+++ kangoeroes.leidingBeheer/Program.cs
@@ -1,6 +1,11 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
+using System.Runtime.InteropServices.ComTypes;
+using kangoeroes.leidingBeheer.Data.Context;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace kangoeroes.leidingBeheer
 {
@@ -9,15 +14,29 @@ namespace kangoeroes.leidingBeheer
     public static void Main(string[] args)
     {
       BuildWebHost(args).Run();
+
     }
 
-    public static IWebHost BuildWebHost(string[] args) =>
-        WebHost.CreateDefaultBuilder(args)
-          .UseKestrel()
-          .UseUrls("http://localhost:5000")
-          .UseContentRoot(Directory.GetCurrentDirectory())
-          .UseIISIntegration()
-          .UseStartup<Startup>()
-          .Build();
+    public static IWebHost BuildWebHost(string[] args)
+    {
+      var host = WebHost.CreateDefaultBuilder(args)
+                .UseKestrel()
+                .UseUrls("http://localhost:5000")
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseIISIntegration()
+                .UseStartup<Startup>()
+                .Build();
+
+      using (var scope = host.Services.CreateScope())
+      {
+        var context = scope.ServiceProvider.GetService<ApplicationDbContext>();
+        context.Database.Migrate();
+      }
+
+      return host;
+    }
+
+
+
   }
 }
