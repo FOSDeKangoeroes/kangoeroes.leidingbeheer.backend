010e4d015b0f8f1c9a61b7ee21326e2d09416ba5
Thomas de Wulf <thomas.de.wulf@telenet.be>
2018-08-09 12:15:38 +0200

    Add documentation

--- kangoeroes.core/Models/Leiding.cs
+++ kangoeroes.core/Models/Leiding.cs
@@ -8,22 +8,50 @@ namespace kangoeroes.core.Models
     /// </summary>
     public class Leiding
     {
+        /// <summary>
+        /// Unieke sleutel van de entiteit.
+        /// </summary>
         public int Id { get; set; }
         
+        /// <summary>
+        /// Unieke sleutel van het gebruikersaccount waaraan de entiteit gekoppeld is.
+        /// </summary>
         public string Auth0Id { get; set; }
         
+        /// <summary>
+        /// Familienaam van de persoon.
+        /// </summary>
         public string Naam { get; set; }
         
+        /// <summary>
+        /// Voornaam van de persoon.
+        /// </summary>
         public string Voornaam { get; set; }
         
+        /// <summary>
+        /// Emailadres van de persoon.
+        /// </summary>
         public string Email { get; set; }
         
+        /// <summary>
+        /// Datum waarop de persoon in de leiding kwam.
+        /// </summary>
         public DateTime LeidingSinds { get; set; }
-        
+         
+        /// <summary>
+        /// Datum waarop de persoon stopte met leiding zijn.
+        /// </summary>
         public DateTime DatumGestopt { get; set; }
         
+        
+        /// <summary>
+        /// Tak waartoe de persoon behoort.
+        /// </summary>
         public Tak Tak { get; set; }
 
+        /// <summary>
+        /// Berekende property die aangeeft of een persoon leiding is geweest.
+        /// </summary>
         [NotMapped]
         public bool HasBeenLeiding => DatumGestopt > LeidingSinds;
 
