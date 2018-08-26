e13334a87ce7a69169e1d6ebf9932d5082105092
Thomas de Wulf <thomas.de.wulf@telenet.be>
2018-08-26 12:27:56 +0200

    Afkorting van dranktype teruggeven

--- kangoeroes.leidingBeheer/ViewModels/PoefViewModels/DrankType/BasicDrankTypeViewModel.cs
+++ kangoeroes.leidingBeheer/ViewModels/PoefViewModels/DrankType/BasicDrankTypeViewModel.cs
@@ -1,9 +1,13 @@
-﻿namespace kangoeroes.leidingBeheer.ViewModels.PoefViewModels.DrankType
+﻿using kangoeroes.leidingBeheer.Helpers;
+
+namespace kangoeroes.leidingBeheer.ViewModels.PoefViewModels.DrankType
 {
   public class BasicDrankTypeViewModel
   {
     public int Id { get; set; }
 
     public string Naam { get; set; }
+
+    public string Afkorting => AfkortingBuilder.BuildAfkorting(Naam);
   }
 }
