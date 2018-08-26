536a5d2b390f0292ef6a84dc4e81c4818f284992
Thomas de Wulf <thomas.de.wulf@telenet.be>
2018-08-26 13:40:58 +0200

    Start uitwerken orders en orderlines

---
+++ kangoeroes.core/Models/Poef/Order.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace kangoeroes.core.Models.Poef
+{
+    public class Order
+    {
+        public Order()
+        {
+            Orderlines = new List<Orderline>();
+        }
+        public int Id { get; set; }
+        
+        public Leiding OrderedBy { get; set; }
+        
+        public DateTime CreatedOn { get; set; }
+        
+        public List<Orderline> Orderlines { get; set; } 
+    }
+}
