5547f57b01803b08dd75d66773ff36b003dade94
Thomas de Wulf <thomas.de.wulf@telenet.be>
2019-11-03 11:11:46 +0100

    Return to initial location in migrations script

--- scripts/addMigration.ps1
+++ scripts/addMigration.ps1
@@ -5,3 +5,5 @@ param(
 Set-Location -Path ..\src
 
 dotnet ef migrations add $migrationName --project "kangoeroes.infrastructure" --startup-project "kangoeroes.webUI"
+
+Set-Location -Path ..\scripts #Return to initial location for less confusion
